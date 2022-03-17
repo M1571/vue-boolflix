739db3622da55b2d5da9ce422c0eca35ca49e909
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-17 22:42:19 +0100

    first data.results

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div>
+      <input type="text" v-model="search">
+    </div>
+
+    <div v-for="movie in movies" :key="movie.id">
+      {{ movie.title }}
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      search: 'ritorno',
+      movies: []
+    }
+  },
+  created() {
+
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=3c426907b15cb93c3c4d0a11f370a038&query=batman')
+    .then( res => {
+      console.log( res.data )
+      this.movies = res.data.results
+    })
+    .catch( error => {
+    console.log( error.response )
+    })
+
   }
 }
 </script>
